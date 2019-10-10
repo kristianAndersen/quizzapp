8cbc66f1079da6ed48d213ef3f558d3b4bbcb493
kristian andersen <krilleandersen@gmail.com>
2019-10-10 15:04:32 +0200

    Add files via upload
    quizz image generator, fonts is bugging a bit

---
+++ js/main.js
@@ -0,0 +1,659 @@
+/*stupid name generator
+var aVocab = new Array();
+var intIncr
+var intCat
+intCat =0
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="FIRST"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="<ONE><FOUR><EIGHT>"
+aVocab[intCat][1][intIncr++]="<TWO><FOUR><EIGHT>"
+aVocab[intCat][1][intIncr++]="<ONE><EIGHT>"
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="ONE"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="Act"
+aVocab[intCat][1][intIncr++]="Afr"
+aVocab[intCat][1][intIncr++]="Ag"
+aVocab[intCat][1][intIncr++]="Agr"
+aVocab[intCat][1][intIncr++]="Alb"
+aVocab[intCat][1][intIncr++]="Am"
+aVocab[intCat][1][intIncr++]="An"
+aVocab[intCat][1][intIncr++]="Angl"
+aVocab[intCat][1][intIncr++]="Ant"
+aVocab[intCat][1][intIncr++]="Aqu"
+aVocab[intCat][1][intIncr++]="Ar"
+aVocab[intCat][1][intIncr++]="Arc"
+aVocab[intCat][1][intIncr++]="As"
+aVocab[intCat][1][intIncr++]="Atl"
+aVocab[intCat][1][intIncr++]="Austr"
+aVocab[intCat][1][intIncr++]="Eb"
+aVocab[intCat][1][intIncr++]="Eg"
+aVocab[intCat][1][intIncr++]="Eng"
+aVocab[intCat][1][intIncr++]="Er"
+aVocab[intCat][1][intIncr++]="Esc"
+aVocab[intCat][1][intIncr++]="Esp"
+aVocab[intCat][1][intIncr++]="Est"
+aVocab[intCat][1][intIncr++]="Eth"
+aVocab[intCat][1][intIncr++]="Etr"
+aVocab[intCat][1][intIncr++]="Eul"
+aVocab[intCat][1][intIncr++]="Euph"
+aVocab[intCat][1][intIncr++]="Eur"
+aVocab[intCat][1][intIncr++]="Ib"
+aVocab[intCat][1][intIncr++]="Ind"
+aVocab[intCat][1][intIncr++]="It"
+aVocab[intCat][1][intIncr++]="Ug"
+aVocab[intCat][1][intIncr++]="Urs"
+aVocab[intCat][1][intIncr++]="Ut"
+aVocab[intCat][1][intIncr++]="Ur"
+aVocab[intCat][1][intIncr++]="Ol"
+aVocab[intCat][1][intIncr++]="Olv"
+aVocab[intCat][1][intIncr++]="Olm"
+aVocab[intCat][1][intIncr++]="Or"
+aVocab[intCat][1][intIncr++]="Orm"
+aVocab[intCat][1][intIncr++]="Ord"
+aVocab[intCat][1][intIncr++]="Os"
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="TWO"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="Bol"
+aVocab[intCat][1][intIncr++]="Br"
+aVocab[intCat][1][intIncr++]="Brund"
+aVocab[intCat][1][intIncr++]="Burg"
+aVocab[intCat][1][intIncr++]="Byth"
+aVocab[intCat][1][intIncr++]="Byz"
+aVocab[intCat][1][intIncr++]="Call"
+aVocab[intCat][1][intIncr++]="Cas"
+aVocab[intCat][1][intIncr++]="Casp"
+aVocab[intCat][1][intIncr++]="Cast"
+aVocab[intCat][1][intIncr++]="Cast"
+aVocab[intCat][1][intIncr++]="Castr"
+aVocab[intCat][1][intIncr++]="Cath"
+aVocab[intCat][1][intIncr++]="Cel"
+aVocab[intCat][1][intIncr++]="Ces"
+aVocab[intCat][1][intIncr++]="Ch"
+aVocab[intCat][1][intIncr++]="Chin"
+aVocab[intCat][1][intIncr++]="Cor"
+aVocab[intCat][1][intIncr++]="Cr"
+aVocab[intCat][1][intIncr++]="Cypr"
+aVocab[intCat][1][intIncr++]="Dal"
+aVocab[intCat][1][intIncr++]="Dam"
+aVocab[intCat][1][intIncr++]="Dan"
+aVocab[intCat][1][intIncr++]="Ferr"
+aVocab[intCat][1][intIncr++]="Flor"
+aVocab[intCat][1][intIncr++]="Gall"
+aVocab[intCat][1][intIncr++]="Gen"
+aVocab[intCat][1][intIncr++]="Germ"
+aVocab[intCat][1][intIncr++]="Gran"
+aVocab[intCat][1][intIncr++]="Helv"
+aVocab[intCat][1][intIncr++]="Hib"
+aVocab[intCat][1][intIncr++]="Jap"
+aVocab[intCat][1][intIncr++]="Jud"
+aVocab[intCat][1][intIncr++]="Laur"
+aVocab[intCat][1][intIncr++]="Lib"
+aVocab[intCat][1][intIncr++]="Lith"
+aVocab[intCat][1][intIncr++]="Luc"
+aVocab[intCat][1][intIncr++]="Lyr"
+aVocab[intCat][1][intIncr++]="Maj"
+aVocab[intCat][1][intIncr++]="Mal"
+aVocab[intCat][1][intIncr++]="Mall"
+aVocab[intCat][1][intIncr++]="Man"
+aVocab[intCat][1][intIncr++]="Manch"
+aVocab[intCat][1][intIncr++]="Mars"
+aVocab[intCat][1][intIncr++]="Merc"
+aVocab[intCat][1][intIncr++]="Min"
+aVocab[intCat][1][intIncr++]="Mir"
+aVocab[intCat][1][intIncr++]="Mol"
+aVocab[intCat][1][intIncr++]="Myr"
+aVocab[intCat][1][intIncr++]="Myt"
+aVocab[intCat][1][intIncr++]="Nam"
+aVocab[intCat][1][intIncr++]="Nar"
+aVocab[intCat][1][intIncr++]="Nor"
+aVocab[intCat][1][intIncr++]="Nub"
+aVocab[intCat][1][intIncr++]="Pan"
+aVocab[intCat][1][intIncr++]="Par"
+aVocab[intCat][1][intIncr++]="Pel"
+aVocab[intCat][1][intIncr++]="Per"
+aVocab[intCat][1][intIncr++]="Pont"
+aVocab[intCat][1][intIncr++]="Rhod"
+aVocab[intCat][1][intIncr++]="Rhyd"
+aVocab[intCat][1][intIncr++]="Rom"
+aVocab[intCat][1][intIncr++]="Russ"
+aVocab[intCat][1][intIncr++]="Sal"
+aVocab[intCat][1][intIncr++]="Sar"
+aVocab[intCat][1][intIncr++]="Scot"
+aVocab[intCat][1][intIncr++]="Seg"
+aVocab[intCat][1][intIncr++]="Senz"
+aVocab[intCat][1][intIncr++]="Sib"
+aVocab[intCat][1][intIncr++]="Spar"
+aVocab[intCat][1][intIncr++]="Sum"
+aVocab[intCat][1][intIncr++]="Sw"
+aVocab[intCat][1][intIncr++]="Sylv"
+aVocab[intCat][1][intIncr++]="Tasm"
+aVocab[intCat][1][intIncr++]="Terr"
+aVocab[intCat][1][intIncr++]="Tes"
+aVocab[intCat][1][intIncr++]="Th"
+aVocab[intCat][1][intIncr++]="Theon"
+aVocab[intCat][1][intIncr++]="Tim"
+aVocab[intCat][1][intIncr++]="Tir"
+aVocab[intCat][1][intIncr++]="Tr"
+aVocab[intCat][1][intIncr++]="Tun"
+aVocab[intCat][1][intIncr++]="Tyr"
+aVocab[intCat][1][intIncr++]="Vir"
+aVocab[intCat][1][intIncr++]="Xan"
+aVocab[intCat][1][intIncr++]="Zan"
+aVocab[intCat][1][intIncr++]="Zeel"
+aVocab[intCat][1][intIncr++]="Zen"
+aVocab[intCat][1][intIncr++]="Zim"
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="FOUR"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="ab"
+aVocab[intCat][1][intIncr++]="ac"
+aVocab[intCat][1][intIncr++]="ad"
+aVocab[intCat][1][intIncr++]="ag"
+aVocab[intCat][1][intIncr++]="al"
+aVocab[intCat][1][intIncr++]="an"
+aVocab[intCat][1][intIncr++]="and"
+aVocab[intCat][1][intIncr++]="ann"
+aVocab[intCat][1][intIncr++]="ant"
+aVocab[intCat][1][intIncr++]="anth"
+aVocab[intCat][1][intIncr++]="ar"
+aVocab[intCat][1][intIncr++]="arc"
+aVocab[intCat][1][intIncr++]="arct"
+aVocab[intCat][1][intIncr++]="ard"
+aVocab[intCat][1][intIncr++]="arn"
+aVocab[intCat][1][intIncr++]="art"
+aVocab[intCat][1][intIncr++]="as"
+aVocab[intCat][1][intIncr++]="at"
+aVocab[intCat][1][intIncr++]="atr"
+aVocab[intCat][1][intIncr++]="av"
+aVocab[intCat][1][intIncr++]="en"
+aVocab[intCat][1][intIncr++]="enn"
+aVocab[intCat][1][intIncr++]="ent"
+aVocab[intCat][1][intIncr++]="er"
+aVocab[intCat][1][intIncr++]="ern"
+aVocab[intCat][1][intIncr++]="es"
+aVocab[intCat][1][intIncr++]="et"
+aVocab[intCat][1][intIncr++]="ev"
+aVocab[intCat][1][intIncr++]="ian"
+aVocab[intCat][1][intIncr++]="iat"
+aVocab[intCat][1][intIncr++]="ib"
+aVocab[intCat][1][intIncr++]="ic"
+aVocab[intCat][1][intIncr++]="iet"
+aVocab[intCat][1][intIncr++]="il"
+aVocab[intCat][1][intIncr++]="in"
+aVocab[intCat][1][intIncr++]="inth"
+aVocab[intCat][1][intIncr++]="is"
+aVocab[intCat][1][intIncr++]="it"
+aVocab[intCat][1][intIncr++]="itr"
+aVocab[intCat][1][intIncr++]="iv"
+aVocab[intCat][1][intIncr++]="on"
+aVocab[intCat][1][intIncr++]="ont"
+aVocab[intCat][1][intIncr++]="op"
+aVocab[intCat][1][intIncr++]="or"
+aVocab[intCat][1][intIncr++]="orc"
+aVocab[intCat][1][intIncr++]="os"
+aVocab[intCat][1][intIncr++]="ov"
+aVocab[intCat][1][intIncr++]="uan"
+aVocab[intCat][1][intIncr++]="uar"
+aVocab[intCat][1][intIncr++]="un"
+aVocab[intCat][1][intIncr++]="und"
+aVocab[intCat][1][intIncr++]="ur"
+aVocab[intCat][1][intIncr++]="yc"
+aVocab[intCat][1][intIncr++]="yg"
+aVocab[intCat][1][intIncr++]="ymn"
+aVocab[intCat][1][intIncr++]="yn"
+aVocab[intCat][1][intIncr++]="yr"
+aVocab[intCat][1][intIncr++]="ys"
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="EIGHT"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="a"
+aVocab[intCat][1][intIncr++]="aea"
+aVocab[intCat][1][intIncr++]="aia"
+aVocab[intCat][1][intIncr++]="ana"
+aVocab[intCat][1][intIncr++]="and"
+aVocab[intCat][1][intIncr++]="as"
+aVocab[intCat][1][intIncr++]="ea"
+aVocab[intCat][1][intIncr++]="ene"
+aVocab[intCat][1][intIncr++]="eos"
+aVocab[intCat][1][intIncr++]="ia"
+aVocab[intCat][1][intIncr++]="ias"
+aVocab[intCat][1][intIncr++]="ica"
+aVocab[intCat][1][intIncr++]="ini"
+aVocab[intCat][1][intIncr++]="ion"
+aVocab[intCat][1][intIncr++]="is"
+aVocab[intCat][1][intIncr++]="ium"
+aVocab[intCat][1][intIncr++]="ius"
+aVocab[intCat][1][intIncr++]="on"
+aVocab[intCat][1][intIncr++]="ona"
+aVocab[intCat][1][intIncr++]="or"
+aVocab[intCat][1][intIncr++]="ova"
+aVocab[intCat][1][intIncr++]="ul"
+aVocab[intCat][1][intIncr++]="um"
+aVocab[intCat][1][intIncr++]="ur"
+aVocab[intCat][1][intIncr++]="us"
+aVocab[intCat][1][intIncr++]="ya"
+aVocab[intCat][1][intIncr++]="ano"
+aVocab[intCat][1][intIncr++]="ar"
+aVocab[intCat][1][intIncr++]="ir"
+	function GenNumber(nRange)
+	{
+                var iNumGen
+		iNumGen = Math.round((Math.random() * nRange/2));
+                return iNumGen;
+	}
+        function GetFrom(aArray)
+	{
+		var undefined
+		var sReturn
+		sReturn = aArray[GenNumber(aArray.length)];
+		if (sReturn == undefined)
+		{
+			sReturn = GetFrom(aArray)
+		}
+		return sReturn
+	}
+	function GetArray(sArrayName)
+	{
+		for (var intLooper=0;intLooper < aVocab.length;intLooper++)
+		{
+			if (aVocab[intLooper][0]==sArrayName)
+			{
+				return aVocab[intLooper][1];
+				break;
+			}
+		}
+	}
+	function ScanLine(sLine)
+	{
+		var iTagStart, iTagEnd
+		var sKey
+		if (sLine.indexOf("<") > -1)
+		{
+			iTagStart = sLine.indexOf("<");
+			iTagEnd = sLine.indexOf(">");
+			
+			sKey = sLine.substr(iTagStart+1, iTagEnd-(iTagStart+1));
+			sKey = GetFrom(GetArray(sKey))
+			sLine = sLine.substr(0, iTagStart) + sKey + sLine.substr(iTagEnd+1, (sLine.length - iTagEnd))
+		}
+		
+		if (sLine.indexOf("<") > - 1)
+		{
+			sLine = ScanLine(sLine)
+		}
+		return sLine;
+	}
+	function GenPlot()
+	{
+		sLine = GetFrom(GetArray("FIRST"));
+		sLine = ScanLine(sLine)
+        return sLine
+		//document.GENFORM.STORY.value = sLine;
+    }
+    
+*/
+document.addEventListener('DOMContentLoaded', function(){
+
+var canvas = window._canvas = new fabric.Canvas('quizzapp');
+
+var rect4 = new fabric.Rect({
+    left: 0,
+    top: 0,
+    width: 540,
+    height: 540,
+    fill: '#ebb11a'
+});
+canvas.add(rect4);
+rect4.setGradient('fill', {
+    type: 'radial',
+    r1: rect4.width / 2,
+    r2: 20,
+    x1: rect4.height / 2,
+    y1: rect4.width / 2,
+    x2: rect4.height / 2,
+    y2: rect4.width / 2,
+    colorStops: {
+        0: '#ebb11a',
+        1: '#dc801d'
+    }
+});
+rect4.selectable=false;
+var txt1 = new fabric.IText("GÆT RIGTIGT OG VIND 200 KRONER!", { 
+    fontFamily: "font90557", 
+    left: canvas.width/2,
+    top: 45,
+    width:450,
+    fontSize: 24,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500
+});
+canvas.add(txt1)
+
+//let txt2 = new fabric.IText(GenPlot()+" vs "+GenPlot(), { 
+    let txt2 = new fabric.IText("Hold1 vs Hold2", {  
+    left: canvas.width/2,
+    top: 100,
+    width:400,
+    height:40,
+    fontSize: 50,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:400,
+    breakWords: false,
+    fontFamily: "font90557"
+});
+canvas.add(txt2)
+var numtxt1 = new fabric.Textbox(" ", { 
+    left: 85,
+    top: 185,
+    width:500,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var numtxt2 = new fabric.Textbox("2", { 
+    fontFamily: "font90557", 
+    left: 270,
+    top: 185,
+    width:480,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var numtxt3 = new fabric.Textbox("3", { 
+    fontFamily: "font90557", 
+    left: 450,
+    top: 185,
+    width:500,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var numtxt4 = new fabric.Textbox("4", { 
+    fontFamily: "font90557", 
+    left: 85,
+    top: 350,
+    width:500,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var numtxt5 = new fabric.Textbox("5", { 
+    fontFamily: "font90557", 
+    left: 270,
+    top: 350,
+    width:500,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var numtxt6 = new fabric.Textbox("6", { 
+    fontFamily: "font90557", 
+    left: 450,
+    top: 350,
+    width:500,
+    height:40,
+    fontSize: 55,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'center',
+    originY: 'center',
+    fixedWidth:500,
+    breakWords: false
+});
+var subtxt1 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 20,
+    top: 210,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+var subtxt2 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 210,
+    top: 210,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+var subtxt3 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 390,
+    top: 210,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+var subtxt4 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 20,
+    top: 380,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+var subtxt5 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 210,
+    top: 380,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+var subtxt6 = new fabric.Textbox("Danmark vinder med mindst syv mål", { 
+    fontFamily: "font86098", 
+    left: 390,
+    top: 380,
+    width:120,
+    fontSize: 15,
+    textAlign: "center",
+    fill: "#000000",
+    hasBorders: false,
+    hasControls: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    originX: 'left',
+    originY: 'top',
+    fixedWidth:500,
+    name:'subtxt'
+});
+
+canvas.add(numtxt1)
+
+canvas.add(numtxt2)
+canvas.add(numtxt3)
+canvas.add(numtxt4)
+canvas.add(numtxt5)
+canvas.add(numtxt6)
+canvas.add(subtxt1)
+canvas.add(subtxt2)
+canvas.add(subtxt3)
+canvas.add(subtxt4)
+canvas.add(subtxt5)
+canvas.add(subtxt6)
+canvas.bringToFront(txt1);
+
+
+numtxt1.set({text:'1'});
+numtxt1.set({fontFamily: 'font90557'});
+
+numtxt2.set({text:'2'});
+numtxt2.set({fontFamily: 'font90557'});
+
+//
+
+
+
+//txt1.set('fontCharacterStyle', 'Caps');
+txt1.selectable=false;
+fabric.loadSVGFromURL("./img/oddsetlogo-01.svg",function(objects,options)
+{
+ 
+  var loadedObjects = fabric.util.groupSVGElements(objects, options);
+  loadedObjects.set({
+    left: 0,
+    top: 460,
+    width:540,
+    height:100
+  });
+  canvas.add(loadedObjects);
+//   /canvas.sendToFront(loadedObjects);
+})
+
+
+canvas.on('object:added', function (opt) {
+             
+    var t1 = opt.target;
+   // console.log(t1.width +' --- '+ t1.fixedWidth)
+   console.log(t1)
+  if (t1.width > t1.fixedWidth) {
+    
+    t1.fontSize *= t1.fixedWidth / (t1.width + 1);  
+t1.width = t1.fixedWidth+50;   
+
+    }
+   
+});
+
+
+canvas.trigger('object:modified', {target: txt2});
+canvas.renderAll();
+var date = new Date();
+var thedate = new Intl.DateTimeFormat('dk-DK').format(date);
+
+var imageSaver = document.getElementById('dl');
+imageSaver.addEventListener('click', saveImage, false);
+function saveImage(e) {
+    this.href =canvas.toDataURL({
+    format: 'png',
+    multiplier: 2
+});
+
+
+this.download = 'Oddsetquizz_'+thedate+'.png'
+console.log("wooo")
+}
+
+
+
+});
